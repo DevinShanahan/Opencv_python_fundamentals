dae14e20a395e015f868ddd5eb32026bdb173c61
DevinShanahan <dshanahan88@gmail.com>
2017-12-08 16:17:19 +0700

    added sample edge detection video

--- edges.py
+++ edges.py
@@ -3,7 +3,7 @@ import cv2
 def nothing(x):
     pass
 
-cap = cv2.VideoCapture('data/india_driving.mp4')
+cap = cv2.VideoCapture('data/india_driving_sample.mp4')
 fourcc = cv2.VideoWriter_fourcc(*'H264') # For fourcc visit fourcc.org
 cap_size = (int(cap.get(3)),int(cap.get(4)))
 out = cv2.VideoWriter('outputs/edges_india_output.avi',fourcc,20,cap_size,False)
@@ -11,8 +11,10 @@ out = cv2.VideoWriter('outputs/edges_india_output.avi',fourcc,20,cap_size,False)
 # Infinite loop until we hit the escape key on keyboard
 while(1):
     ret, frame = cap.read()
+    if not ret:
+        break
     # get current positions of four trackbars
-    lower = 70
+    lower = 140
     upper = 220
     edges = cv2.Canny(frame, lower, upper)
 
